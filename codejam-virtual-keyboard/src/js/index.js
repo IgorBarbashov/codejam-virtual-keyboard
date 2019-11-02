88d56d97a448fac270f20afcb206da56885f970a
isbnn <isb.cv@yandex.ru>
2019-11-02 22:51:20 +0300

    feat: add keyborad's layout in json & implement render keyboard

--- codejam-virtual-keyboard/src/js/index.js
+++ codejam-virtual-keyboard/src/js/index.js
@@ -1,1 +1,34 @@
+import keyboardLayout from '../assets/keyboard.json';
+
+const state = {
+  language: 'eng',
+  shifting: 'unshift'
+};
+
 // ff.getSVGDocument().querySelector("text").textContent = 99
+// document.body.addEventListener('keydown', e => {
+//   console.log(`${e.key} - ${e.code}`);
+// });
+
+// window.body
+
+const keyboardElement = document.createElement('div');
+keyboardElement.classList.add('keyboard');
+
+keyboardLayout.forEach((row, i) => {
+  const keyboardRow = document.createElement('div');
+  keyboardRow.classList.add('keyboard-row');
+
+  row.forEach((el, j) => {
+    const keyboardKey = document.createElement('div');
+    keyboardKey.setAttribute('id', el.id);
+    keyboardKey.classList.add('keyboard-key');
+    el.class && keyboardKey.classList.add(el.class);
+    keyboardKey.textContent = el.label ? el.label : el[`${state.language}-${state.shifting}`];
+    keyboardRow.appendChild(keyboardKey);
+  });
+
+  keyboardElement.appendChild(keyboardRow);
+});
+
+document.body.appendChild(keyboardElement);
