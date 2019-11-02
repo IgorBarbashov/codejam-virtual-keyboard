3889b862ccc959c6849f1ba8e32e75064cc15eab
isbnn <isb.cv@yandex.ru>
2019-11-03 00:21:59 +0300

    feat: implement languages switch & mark pressed keys

--- codejam-virtual-keyboard/src/js/index.js
+++ codejam-virtual-keyboard/src/js/index.js
@@ -2,33 +2,63 @@ import keyboardLayout from '../assets/keyboard.json';
 
 const state = {
   language: 'eng',
-  shifting: 'unshift'
+  shifting: 'unshift',
+  activeKeys: [],
+  pressedLangSwitches: []
 };
-
-// ff.getSVGDocument().querySelector("text").textContent = 99
-// document.body.addEventListener('keydown', e => {
-//   console.log(`${e.key} - ${e.code}`);
-// });
-
-// window.body
 
 const keyboardElement = document.createElement('div');
 keyboardElement.classList.add('keyboard');
+document.body.appendChild(keyboardElement);
 
-keyboardLayout.forEach((row, i) => {
-  const keyboardRow = document.createElement('div');
-  keyboardRow.classList.add('keyboard-row');
+function renderKeyboard() {
+  keyboardElement.innerHTML = null;
 
-  row.forEach((el, j) => {
-    const keyboardKey = document.createElement('div');
-    keyboardKey.setAttribute('id', el.id);
-    keyboardKey.classList.add('keyboard-key');
-    el.class && keyboardKey.classList.add(el.class);
-    keyboardKey.textContent = el.label ? el.label : el[`${state.language}-${state.shifting}`];
-    keyboardRow.appendChild(keyboardKey);
+  keyboardLayout.forEach((row, i) => {
+    const keyboardRow = document.createElement('div');
+    keyboardRow.classList.add('keyboard-row');
+
+    row.forEach((el, j) => {
+      const keyboardKey = document.createElement('div');
+      keyboardKey.setAttribute('id', el.id);
+      keyboardKey.classList.add('keyboard-key');
+      el.class && keyboardKey.classList.add(el.class);
+      state.activeKeys.includes(el.id) && keyboardKey.classList.add('active');
+      keyboardKey.textContent = el.label ? el.label : el[`${state.language}-unshift`];
+      keyboardRow.appendChild(keyboardKey);
+    });
+
+    keyboardElement.appendChild(keyboardRow);
   });
+}
 
-  keyboardElement.appendChild(keyboardRow);
+window.addEventListener('keydown', e => {
+  if (e.repeat) {
+    return;
+  }
+  if (e.code === 'ShiftLeft' || e.code === 'ControlLeft') {
+    state.pressedLangSwitches.push(e.code);
+  }
+  if (e.code === 'ShiftLeft') {
+    state.shifting = state.shifting === 'unshift' ? 'shift' : 'unshift';
+  }
+  state.activeKeys.push(e.code);
+  renderKeyboard();
 });
 
-document.body.appendChild(keyboardElement);
+window.addEventListener('keyup', e => {
+  const wasPressed = state.pressedLangSwitches.length;
+  if (e.code === 'ShiftLeft' || e.code === 'ControlLeft') {
+    state.pressedLangSwitches = state.pressedLangSwitches.filter(el => el !== e.code);
+  }
+  if (wasPressed === 2) {
+    state.language = state.language === 'eng' ? 'rus' : 'eng';
+  }
+  if (e.code === 'ShiftLeft') {
+    state.shifting = 'unshift';
+  }
+  state.activeKeys = state.activeKeys.filter(el => el !== e.code);
+  renderKeyboard();
+});
+
+renderKeyboard();
